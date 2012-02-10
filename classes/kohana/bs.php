4c21391c52f096eb52d89979b03bdf41dc11ad0b
John Chipps-Harding <digitaljohn@me.com>
2012-02-10 17:56:20 +0000

    Added first version...
    Signed-off-by: John Chipps-Harding <digitaljohn@me.com>

---
+++ classes/kohana/bs.php
@@ -0,0 +1,48 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Kohana_BS
+{
+
+	public static function input($name, $value, $errors, array $attributes = NULL)
+	{
+		$form_element = Form::input($name, $value, $attributes);
+		return BS::wrap($name, $form_element, $errors);
+	}
+	
+	public static function password($name, $value, $errors, array $attributes = NULL)
+	{
+		$form_element = Form::password($name, $value, $attributes);
+		return BS::wrap($name, $form_element, $errors);
+	}
+	
+	public static function textarea($name, $value, $errors, array $attributes = NULL)
+	{
+		$form_element = Form::textarea($name, $value, $attributes);
+		return BS::wrap($name, $form_element, $errors);
+	}
+	
+	public static function select($name, array $options, $selected, $errors, array $attributes = NULL)
+	{
+		$form_element = Form::select($name, $options, $selected, $attributes);
+		return BS::wrap($name, $form_element, $errors);
+	}
+	
+	public static function wrap($name, $form_element, $errors)
+	{
+		$isError = Arr::get($errors, $name) != NULL;
+		
+		$errorClass = $isError ? ' error' : '';
+	
+		$out = '';
+		$out.= '<div class="control-group'.$errorClass.'">';
+		$out.= '<label for="'.__($name).'" class="control-label">'.__($name).'</label>';
+		$out.= '<div class="controls">';
+        $out.= 	$form_element;
+        if($isError) $out.= '<p class="help-block">'.Arr::get($errors, $name).'</p>';
+        $out.= '</div>';
+        $out.= '</div>';
+
+		return $out;
+	}
+	
+}
