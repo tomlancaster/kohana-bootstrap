c5fcd1c8cd74ec70f37ed0f0d277d8d5f3b80a8c
John Chipps-Harding <digitaljohn@me.com>
2012-02-15 21:22:28 +0000

    Docs
    Added inline documentation

--- classes/kohana/bootstrap.php
+++ classes/kohana/bootstrap.php
@@ -2,47 +2,100 @@
 
 class Kohana_Bootstrap
 {
-
-	public static function input($name, $value, $errors, array $attributes = NULL)
+	/**
+	 * Creates a form input. If no type is specified, a "text" type input will
+	 * be returned.
+	 *
+	 *     echo Bootstrap::input('username', $username);
+	 *
+	 * @param   string  input name
+	 * @param   string  input value
+	 * @param   array   errors
+	 * @param   array   html attributes
+	 * @return  string
+	 */
+	public static function input($name, $value, $errors = NULL, array $attributes = NULL)
 	{
-		$form_element = Form::input($name, $value, $attributes);
-		return Bootstrap::wrap($name, $form_element, $errors);
+		return Bootstrap::wrap($name, Form::input($name, $value, $attributes), $errors);
 	}
 	
-	public static function password($name, $value, $errors, array $attributes = NULL)
+	/**
+	 * Creates a password form input.
+	 *
+	 *     echo Bootstrap::password('password');
+	 *
+	 * @param   string  input name
+	 * @param   string  input value
+	 * @param   array   errors
+	 * @param   array   html attributes
+	 * @return  string
+	 */
+	public static function password($name, $value, $errors = NULL, array $attributes = NULL)
 	{
-		$form_element = Form::password($name, $value, $attributes);
-		return Bootstrap::wrap($name, $form_element, $errors);
+		return Bootstrap::wrap($name, Form::password($name, $value, $attributes), $errors);
 	}
 	
-	public static function textarea($name, $value, $errors, array $attributes = NULL)
+	/**
+	 * Creates a textarea form input.
+	 *
+	 *     echo Bootstrap::textarea('about', $about);
+	 *
+	 * @param   string   textarea name
+	 * @param   string   textarea body
+	 * @param   array    errors
+	 * @param   array    html attributes
+	 * @param   boolean  encode existing HTML characters
+	 * @return  string
+	 */
+	public static function textarea($name, $value, $errors = NULL, array $attributes = NULL, $double_encode = TRUE)
 	{
-		$form_element = Form::textarea($name, $value, $attributes);
-		return Bootstrap::wrap($name, $form_element, $errors);
+		return Bootstrap::wrap($name, Form::textarea($name, $value, $attributes, $double_encode), $errors);
 	}
 	
-	public static function select($name, array $options, $selected, $errors, array $attributes = NULL)
+	/**
+	 * Creates a select form input.
+	 *
+	 *     echo Bootstrap::select('country', $countries, $country);
+	 *
+	 * @param   string   input name
+	 * @param   array    available options
+	 * @param   mixed    selected option string, or an array of selected options
+	 * @param   array    errors
+	 * @param   array    html attributes
+	 * @return  string
+	 */
+	public static function select($name, array $options, $selected, $errors = NULL, array $attributes = NULL)
 	{
-		$form_element = Form::select($name, $options, $selected, $attributes);
-		return Bootstrap::wrap($name, $form_element, $errors);
+		return Bootstrap::wrap($name, Form::select($name, $options, $selected, $attributes), $errors);
 	}
 	
-	public static function wrap($name, $form_element, $errors)
+	/**
+	 * Wraps a form element with Boostrap specific HTML.
+	 *
+	 *     echo Bootstrap::select('country', $countries, $country);
+	 *
+	 * @param   string   form item name
+	 * @param   string   html form element
+	 * @param   array    errors
+	 * @return  string
+	 */
+	public static function wrap($name, $form_element, $errors = NULL)
 	{
-		$isError = Arr::get($errors, $name) != NULL;
+		$is_error = ($errors != NULL) && (Arr::get($errors, $name) != NULL);
+		$error_class = $is_error ? ' error' : '';
+		$error_html = $is_error ? '<p class="help-block">'.Arr::get($errors, $name).'</p>' : '';
 		
-		$errorClass = $isError ? ' error' : '';
-	
-		$out = '';
-		$out.= '<div class="control-group'.$errorClass.'">';
-		$out.= '<label for="'.__($name).'" class="control-label">'.__($name).'</label>';
-		$out.= '<div class="controls">';
-        $out.= 	$form_element;
-        if($isError) $out.= '<p class="help-block">'.Arr::get($errors, $name).'</p>';
-        $out.= '</div>';
-        $out.= '</div>';
-
+		$i18n_name = __($name);
+		
+		$out = <<<OUT
+<div class="control-group{$error_class}">
+	<label for="{$i18n_name}" class="control-label">{$i18n_name}</label>
+	<div class="controls">
+		{$form_element}{$error_html}
+	</div>
+</div>
+OUT;
+		
 		return $out;
 	}
-	
 }
